35247bfa710eee28c2f943c93da7dca976adff0f
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-05-16 18:40:49 +0530

    design home page

---
+++ lib/pages/home.dart
@@ -0,0 +1,50 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:insta_solve/widgets/instasolve_app_bar.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const InstasolveAppBar(),
+
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.image_search,
+              color: Theme.of(context).colorScheme.onPrimaryContainer,
+              size: 164,
+            ),
+            SizedBox(height: 50),
+            Text(
+              "Start scanning to save your answers",
+              style: Theme.of(context).textTheme.titleLarge?.copyWith(fontWeight: FontWeight.w700),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Your saved answers will appear here",
+              style: Theme.of(context).textTheme.labelSmall?.copyWith(color: Colors.grey.shade600),
+            ),
+            SizedBox(height: 80),
+          ],
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton.extended(
+        tooltip: "Scan Picture",
+        label: Text("Scan"),
+        icon: Icon(Icons.camera_alt_rounded),
+        onPressed: () {
+          Navigator.pushNamed(context, "/scan");
+        }
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+    );
+  }
+}
