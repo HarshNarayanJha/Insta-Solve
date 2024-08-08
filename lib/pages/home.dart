3c92aa42115bdc7dcfd12fa6ebcc28b60f5c8a23
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-08-08 19:00:40 +0530

    feat: add search functionallity to home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -26,11 +26,14 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   ScrollController controller = ScrollController();
+  TextEditingController searchController = TextEditingController();
+  FocusNode searchFocus = FocusNode();
   bool fabVisible = true;
 
   String filterSubject = 'all';
   String filterClass = 'all';
   bool sortOldFirst = false;
+  String searchText = '';
 
   @override
   void initState() {
@@ -85,7 +88,8 @@ class _HomePageState extends State<HomePage> {
     //   return Center(child: Container(child: Text("App evalution has ended. Uninstall this version and contact the dev to get a new version")));
     // }
 
-    bool filterApplied = filterClass != 'all' || filterSubject != 'all' || sortOldFirst;
+    bool filterApplied =
+        filterClass != 'all' || filterSubject != 'all' || sortOldFirst;
 
     return Scaffold(
       appBar: const InstasolveAppBar(),
@@ -94,51 +98,84 @@ class _HomePageState extends State<HomePage> {
           builder: (context, box, widget) {
             return Responsive(
               child: Container(
-                padding: const EdgeInsets.only(top: 16.0, left: 16.0, right: 16.0),
+                padding:
+                    const EdgeInsets.only(top: 16.0, left: 16.0, right: 16.0),
                 child: Column(
                   children: [
                     const SizedBox(height: 5),
                     Visibility(
                       visible: box.values.isNotEmpty,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
+                      child: Column(
                         children: [
-                          Text(
-                            "Saved Answers",
-                            style: Theme.of(context).textTheme.headlineLarge,
-                          ),
-                          const Spacer(),
-                          IconButton(
-                            icon: const Icon(Icons.filter_alt_rounded),
-                            color:
-                                filterApplied
-                                    ? Colors.green
-                                    : null,
-                            onPressed: () {
-                              showModalBottomSheet(
-                                context: context,
-                                barrierColor: Colors.black45,
-                                sheetAnimationStyle: AnimationStyle(
-                                    duration: Durations.medium4,
-                                    curve: Curves.easeIn,
-                                    reverseCurve: Curves.easeOut,
-                                    reverseDuration: Durations.short4),
-                                showDragHandle: true,
-                                useSafeArea: true,
-                                builder: (context) {
-                                  return FilterSheet(
-                                    filterSubject: filterSubject,
-                                    filterClass: filterClass,
-                                    sortOldFirst: sortOldFirst,
-                                    onFilterClass: onFilterClass,
-                                    onFilterSubject: onFilterSubject,
-                                    onSortOldFirst: onSortOldFirst,
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Saved Answers",
+                                style:
+                                    Theme.of(context).textTheme.headlineLarge,
+                              ),
+                              const Spacer(),
+                              IconButton(
+                                icon: const Icon(Icons.filter_alt_rounded),
+                                color: filterApplied ? Colors.green : null,
+                                onPressed: () {
+                                  showModalBottomSheet(
+                                    context: context,
+                                    barrierColor: Colors.black45,
+                                    sheetAnimationStyle: AnimationStyle(
+                                        duration: Durations.medium1,
+                                        curve: Curves.easeIn,
+                                        reverseCurve: Curves.easeOut,
+                                        reverseDuration: Durations.short4),
+                                    showDragHandle: true,
+                                    useSafeArea: true,
+                                    builder: (context) {
+                                      return FilterSheet(
+                                        filterSubject: filterSubject,
+                                        filterClass: filterClass,
+                                        sortOldFirst: sortOldFirst,
+                                        onFilterClass: onFilterClass,
+                                        onFilterSubject: onFilterSubject,
+                                        onSortOldFirst: onSortOldFirst,
+                                      );
+                                    },
                                   );
                                 },
-                              );
-                            },
+                              ),
+                              const SizedBox(width: 10),
+                            ],
                           ),
-                          const SizedBox(width: 10),
+                          const SizedBox(height: 5),
+                          TextField(
+                            controller: searchController,
+                            focusNode: searchFocus,
+                            autofocus: false,
+                            onChanged: (value) {
+                              setState(() {
+                                searchText = value;
+                              });
+                            },
+                            decoration: InputDecoration(
+                              suffixIcon: searchText.isEmpty
+                                  ? const Icon(Icons.search)
+                                  : IconButton(
+                                      icon: const Icon(Icons.clear),
+                                      onPressed: () {
+                                        setState(() {
+                                          searchText = "";
+                                          searchController.clear();
+                                          searchFocus.unfocus();
+                                        });
+                                      },
+                                    ),
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(35)),
+                              label: const Text("Search Your Questions"),
+                              isDense: true,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
                         ],
                       ),
                       // child: AnswerFilterWidget(
@@ -159,7 +196,10 @@ class _HomePageState extends State<HomePage> {
                                 if (sortOldFirst) {
                                   ans = box.values.toList()[index];
                                 } else {
-                                  ans = box.values.toList().reversed.toList()[index];
+                                  ans = box.values
+                                      .toList()
+                                      .reversed
+                                      .toList()[index];
                                 }
 
                                 if (filterClass != 'all') {
@@ -171,6 +211,13 @@ class _HomePageState extends State<HomePage> {
                                   if (ans.subject != filterSubject) {
                                     return const SizedBox(height: 0);
                                   }
+                                }
+
+                                if (searchText.isNotEmpty &&
+                                    !ans.prompt
+                                        .toLowerCase()
+                                        .contains(searchText.toLowerCase())) {
+                                  return const SizedBox(height: 0);
                                 }
 
                                 return AnswerCardWidget(
